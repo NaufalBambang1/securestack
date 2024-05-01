8253dd43e5da7280c844cdf4bc1242176be2189d
NaufalBambang1 <schartregion@gmail.com>
2024-05-01 09:58:36 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <table> 
+                        <tr class="p-6">
+                            <th class="w-20">Log ID</th>
+                            <th class="w-50">User ID</th>
+                            <th>Locker ID</th>
+                            <th>Access Time</th>
+                            <th>Access Method</th>
+                            <th>Access Result</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
